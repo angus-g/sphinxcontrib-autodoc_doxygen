15517697a93859a9e4c6b3bc038e3a47621bdbce
Angus Gibson <angus@agibson.me>
2017-03-16 08:25:12 -0400

    Don't use :returns: field in lists for Fortran
    I think this field needs type information in the Fortran domain, which
    we don't have from Doxygen yet, so as previously implemented this
    would cause an exception.

--- sphinxcontrib/autodoc_doxygen/xmlutils.py
+++ sphinxcontrib/autodoc_doxygen/xmlutils.py
@@ -124,7 +124,9 @@ class _DoxygenXmlParagraphFormatter(object):
 
     def visit_simplesect(self, node):
         if node.get('kind') == 'return':
-            self.lines.append(':returns: ')
+            # XXX we need  type information here or the fortran domain crashes
+            #self.lines.append(':returns: ')
+            self.lines.append('returns: ')
             self.continue_line = True
         self.generic_visit(node)
 
